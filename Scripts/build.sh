47eae5e3c20f551f81217585fd49898f4d4140ed
chrisakira <chris.akira.maekawa@gmail.com>
2023-01-21 18:39:34 +0000

    commit inicial

---
+++ Scripts/build.sh
@@ -0,0 +1,7 @@
+sudo docker-compose -f /home/vm2/Home-Lab/chronograf/docker-compose.yml  up -d
+sudo docker-compose -f /home/vm2/Home-Lab/grafana/docker-compose.yml  up -d
+sudo docker-compose -f /home/vm2/Home-Lab/influx/docker-compose.yml  up -d
+sudo docker-compose -f /home/vm2/Home-Lab/portainer/docker-compose.yml  up -d
+sudo docker-compose -f /home/vm2/Home-Lab/node-red/docker-compose.yml  up -d
+sudo docker-compose -f /home/vm2/Home-Lab/home-assistant/docker-compose.yml  up -d
+
