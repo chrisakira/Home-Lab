8a1d7139cf9e70ba16dc332dd70430f58bfba566
chrisakira <chris.akira.maekawa@gmail.com>
2023-03-05 02:45:17 +0000

    alguma coisa aconteceu e eu não sei o que

---
+++ Scripts/build_KBB.sh
@@ -0,0 +1,4 @@
+sudo docker-compose -f /home/vm2/Home-Lab/chronograf/docker-compose.yml up --build -d
+sudo docker-compose -f /home/vm2/Home-Lab/grafana/docker-compose.yml up --build -d
+sudo docker-compose -f /home/vm2/Home-Lab/influx/docker-compose.yml up --build -d 
+sudo docker-compose -f /home/vm2/Home-Lab/node-red/docker-compose.yml up --build -d 
