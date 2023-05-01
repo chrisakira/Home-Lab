6b39bf3f96bd7cd705b7033ef74839640c8719e2
chrisakira <chris.akira.maekawa@gmail.com>
2023-05-01 00:04:44 +0000

    algo aconteceu

--- Scripts/build.sh
+++ Scripts/build.sh
@@ -1,9 +1,4 @@
-sudo docker-compose -f /home/vm2/Home-Lab/chronograf/docker-compose.yml  up --build -d
-sudo docker-compose -f /home/vm2/Home-Lab/AD_Guard/docker-compose.yml up -d
-sudo docker-compose -f /home/vm2/Home-Lab/grafana/docker-compose.yml  up --build -d
-sudo docker-compose -f /home/vm2/Home-Lab/influx/docker-compose.yml  up --build -d
-sudo docker-compose -f /home/vm2/Home-Lab/portainer/docker-compose.yml  up --build -d
-sudo docker-compose -f /home/vm2/Home-Lab/node-red/docker-compose.yml  up --build -d
-sudo docker-compose -f /home/vm2/Home-Lab/home-assistant/docker-compose.yml up --build -d
+clear 
+sudo docker-compose -f /home/vm2/Home-Lab/AD_Guard/docker-compose.yml up -d  
 sudo docker-compose -f /home/vm2/Home-Lab/zomboid-dedicated-server/docker-compose.yaml up -d
 
--- Scripts/build_KBB.sh
+++
@@ -1,4 +0,0 @@
-sudo docker-compose -f /home/vm2/Home-Lab/chronograf/docker-compose.yml up --build -d
-sudo docker-compose -f /home/vm2/Home-Lab/grafana/docker-compose.yml up --build -d
-sudo docker-compose -f /home/vm2/Home-Lab/influx/docker-compose.yml up --build -d 
-sudo docker-compose -f /home/vm2/Home-Lab/node-red/docker-compose.yml up --build -d 
